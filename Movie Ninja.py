76a6324c31145cac3f4a2653cb7ddce8b28ceb2d
dkollmar <davida.kollmar@gmail.com>
2018-11-06 17:32:10 -0500

    Update Movie Ninja.py

--- Movie Ninja.py
+++ Movie Ninja.py
@@ -10,32 +10,35 @@ import pandas as pd
 # In[147]:
 
 
-metadata=pd.read_csv("movie_metadata_with_score_metacritic.csv", index_col="Unnamed: 0")
-metadata=metadata.loc[~metadata["metacritic_metascore"].isna()]
-metadata=metadata.drop(["homepage","keywords","overview","status","tagline","imdb_metascore"],1)
-metadata=metadata.drop_duplicates()
-metadata["release_year"]=metadata.release_date.str[0:4].astype(int)
+metadata=pd.read_csv("movie_metadata_with_score_metacritic.csv", index_col="Unnamed: 0") #reading in metacritic dataset
+metadata=metadata.loc[~metadata["metacritic_metascore"].isna()] #removing rows with no metacritic data
+metadata=metadata.drop(["homepage","keywords","overview","status","tagline","imdb_metascore"],1) #dropping unnecessary features
+metadata=metadata.drop_duplicates() #dropping duplicates
+metadata["release_year"]=metadata.release_date.str[0:4].astype(int) #creating new column with year for comparison
 
 
 # In[131]:
 
 
-rev_data=pd.read_csv("Revenue.csv")
+rev_data=pd.read_csv("Revenue.csv") #reading in revenue data
+
+#Adjusting units
 rev_data["Budget"]=rev_data["Budget($M)"]*1000000
-rev_data["Worldwide Gross"]=rev_data["Worldwide Gross($M)"]*1000000
+rev_data["Worldwide Gross"]=rev_data["Worldwide Gross($M)"]*1000000 
 rev_data["Domestic Gross"]=rev_data["Domestic Gross($M)"]*1000000
 rev_data=rev_data.drop(["Budget($M)","Domestic Gross($M)","Worldwide Gross($M)"],1)
-rev_data=rev_data.rename(columns={"Movie":"title"})
-rev_data=rev_data.drop_duplicates()
+
+rev_data=rev_data.rename(columns={"Movie":"title"})#renaming column to match metadata
+rev_data=rev_data.drop_duplicates()#dropping duplicates
 
 
 # In[102]:
 
 
-critic_revenue=metadata.merge(rev_data,on="title")
-critic_revenue=critic_revenue.drop_duplicates()
-critic_revenue=critic_revenue.loc[(critic_revenue["Worldwide Gross"]!=0) | (critic_revenue["revenue"]!=0)]
-
+critic_revenue=metadata.merge(rev_data,on="title") #merging datasets
+critic_revenue=critic_revenue.drop_duplicates() #dropping duplicates
+critic_revenue=critic_revenue.loc[(critic_revenue["Worldwide Gross"]!=0) | (critic_revenue["revenue"]!=0)] #removing rows with no revenue data
+critic_revenue=critic_revenue.loc[(np.abs(critic_revenue.release_year-critic_revenue.Year)<=5)] #removing rows where the years don't match, as this indicates different movies
 
 #Normalizing by the median for merged data set
 
@@ -48,4 +51,3 @@ critic_revenue["revenue"]=(metadata["revenue"]-metadata["revenue"].median())/met
 critic_revenue["vote_count"]=(metadata["vote_count"]-metadata["vote_count"].median())/metadata["vote_count"].std()
 
 
-critic_revenue=critic_revenue.loc[(np.abs(critic_revenue.release_year-critic_revenue.Year)<=5)]
